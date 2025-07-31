d05d6b343b566102bfe048ed6fd2807827ba13cb
hwop <100109777+hwop@users.noreply.github.com>
2025-07-31 22:01:32 +0530

    Update bot.py

--- ANNIEMUSIC/core/bot.py
+++ ANNIEMUSIC/core/bot.py
@@ -11,7 +11,7 @@ from ..logging import LOGGER
 class JARVIS(Client):
     def __init__(self):
         super().__init__(
-            name="AnnieXMusic",
+            name="Hw Music Bot",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             bot_token=config.BOT_TOKEN,
